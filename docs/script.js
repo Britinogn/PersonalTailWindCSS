65fe102eec5308cca2ee64604626c0e3505b25dd
Tino Nova Tech <124672998+Britinogn@users.noreply.github.com>
2025-07-12 10:01:39 -0500

    first commit

---
+++ docs/script.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById('mobile-menu-btn');
+const menu = document.getElementById('mobile-menu');
+
+btn.addEventListener('click', () => {
+menu.classList.toggle('hidden');
+});
+
+
+
+
+
+
+  window.addEventListener('scroll', function () {
+    const header = document.getElementById('header');
+    if (window.scrollY > 10) {
+      header.classList.add('bg-indigo-900', 'shadow', 'text-gray-800');
+      header.classList.remove('text-white');
+    } else {
+      header.classList.remove('bg-indigo-900', 'shadow', 'text-gray-800');
+      header.classList.add('text-white');
+    }
+  });
+
